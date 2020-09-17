dd3702525bffc37ed08d3e9f582769dbb7c67698
cattree <morestarforyou@gmail.com>
2020-09-17 16:09:12 +0800

    Update utils.py
    合并文件 优化文件树读取

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import xarray as xr
+from tqdm import tqdm
 
 
 def load_nc_data(path: str):
@@ -104,16 +105,21 @@ def make_dataframe(data, time, csv_name, data_column_name, subdir_name):
     :param subdir_name: 子文件夹名称
     :return:
     """
-    processed_path = os.path.join('processed_data', subdir_name)
+    base_path = 'processed_data'
+    processed_path = os.path.join(base_path, subdir_name)
     try:
-        df = pd.read_csv(os.path.join(processed_path, csv_name), index_col=0)
-        df[data_column_name] = data
-
-        df.to_csv(os.path.join(processed_path, csv_name))
-    except FileNotFoundError:
-        data_dict = {'time': time, data_column_name: data}
-        df = pd.DataFrame(data_dict)
-        df.to_csv(os.path.join(processed_path, csv_name))
+        os.mkdir(processed_path)
+    except FileExistsError:
+        return
+    finally:
+        try:
+            df = pd.read_csv(os.path.join(processed_path, csv_name), index_col=0)
+            df[data_column_name] = data
+            df.to_csv(os.path.join(processed_path, csv_name))
+        except FileNotFoundError:
+            data_dict = {'time': time, data_column_name: data}
+            df = pd.DataFrame(data_dict)
+            df.to_csv(os.path.join(processed_path, csv_name))
 
 
 def make_csv_file(dataset, lat, lon, subdir_name):
@@ -138,7 +144,59 @@ def make_csv_file(dataset, lat, lon, subdir_name):
             make_dataframe(data, t, f'lat-{i}lon-{j}.csv', attr[1], subdir_name)
 
 
-# if __name__ == '__main__':
-#     d = load_nc_data(r'G:\nc\04\ww3.201004_dp.nc')
-#     _lat, _lon = get_part_lat_lon(d, (34.75, 35.5), (120.5, 123.5))
-#     make_csv_file(d, _lat, _lon)
+def get_all_file(root_path):
+    """
+    获得根目录下所有文件
+    :param root_path: 根目录
+    :return: all_files_path_list[list]
+    """
+    # 三个返回值
+    # folder: 全部的文件路径
+    # subs: 子文件夹列表
+    # files: 最深目录下的文件
+    all_files_path_list = list()
+    sub_dir_list = list()
+    flag = True
+    for folder, subdir, files in os.walk(root_path):
+        if not flag:
+            for item in files:
+                full_file_path = os.path.join(folder, item)
+                all_files_path_list.append(full_file_path)
+        flag = False
+        if subdir:
+            sub_dir_list.append(subdir)
+
+    return all_files_path_list, sub_dir_list[0]
+
+
+def merge_csv_file(root_path):
+    """
+    合并csv文件
+
+    :param root_path: 根路径
+    :return:
+    """
+
+    df = pd.DataFrame()
+    all_files, _ = get_all_file(root_path)
+    saved_path = os.path.join(root_path, 'merge_files')
+    try:
+        os.mkdir(saved_path)
+    except FileExistsError:
+        os.remove(saved_path)
+    finally:
+        for file_path in tqdm(all_files):
+            file_name = file_path.split('\\')
+
+            new_file_path = os.path.join(file_name[0], os.path.join('merge_files', file_name[-1]))
+            content = pd.read_csv(file_path, index_col=0)
+            df = pd.concat([df, content])
+            df.to_csv(new_file_path)
+
+
+if __name__ == '__main__':
+#     get_all_file('../raw_data')
+    merge_csv_file('../processed_data')
+    # d = load_nc_data(r'G:\nc\04\ww3.201004_dp.nc')
+    # _lat, _lon = get_part_lat_lon(d, (34.75, 35.5), (120.5, 123.5))
+    # make_csv_file(d, _lat, _lon)

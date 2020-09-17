d211843ba5d5315730877d08f2ecfba40cf92b0d
cattree <morestarforyou@gmail.com>
2020-09-17 16:08:59 +0800

    Update main.py
    添加按月合并文件

--- main.py
+++ main.py
@@ -2,32 +2,26 @@ from utils.utils import *
 from tqdm import tqdm
 
 
-def get_dir_list(root_dir_path):
-    """
-    获取文件列表
-    :param root_dir_path: 根目录
-    :return: list
-    """
-    processed_path = 'processed_data'
-    for sub_dir in os.listdir(root_dir_path):
-        try:
-            os.mkdir(os.path.join(processed_path, sub_dir))
-        except FileExistsError:
-            pass
-    return os.listdir(root_dir_path)
-
-
 if __name__ == '__main__':
 
-    start_lon = (120.5, 123.5)
-    start_lat = (34.75, 35.5)
-
+    start_lon = (120.5, 124.5)
+    start_lat = (34.75, 36.5)
     root_path = 'raw_data'
-    dir_list = get_dir_list(root_path)
-    print('处理进度:\n')
-    for subdir in tqdm(dir_list):
-        file_list = os.listdir(os.path.join(root_path, subdir))
-        for file in file_list:
-            data = load_nc_data(os.path.join(os.path.join(root_path, subdir), file))
-            _lat, _lon = get_part_lat_lon(data, start_lat, start_lon)
-            make_csv_file(data, _lat, _lon, subdir)
+    process_path = 'processed_data'
+    all_files_path_list, subdir_list = get_all_file(root_path)
+
+    try:
+        os.mkdir(process_path)
+    except FileExistsError:
+        os.remove(process_path)
+    finally:
+        for file, sub in tqdm(zip(all_files_path_list, subdir_list), total=len(subdir_list)):
+            # 获取全球数据集
+            dataset = load_nc_data(file)
+            # 获取区间内部分经纬度
+            _lat, _lon = get_part_lat_lon(dataset, start_lat, start_lon)
+            # 制作csv文件
+            make_csv_file(dataset, _lat, _lon, sub)
+        # 按月合并csv文件
+        merge_csv_file(process_path)
+

f6110bf6353dc1880e59055ec64f1fca4b8889bc
cattree <morestarforyou@gmail.com>
2020-09-27 17:30:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-from utils.utils import *
-from tqdm import tqdm
-
+import os
+import shutil
+from utils.utils import OperateFiles
 
 if __name__ == '__main__':
-
-    start_lon = (120.5, 124.5)
-    start_lat = (34.75, 36.5)
-    root_path = 'raw_data'
-    process_path = 'processed_data'
-    all_files_path_list, subdir_list = get_all_file(root_path)
-
     try:
-        os.mkdir(process_path)
+        os.mkdir('processed')
     except FileExistsError:
-        os.remove(process_path)
-    finally:
-        for file, sub in tqdm(zip(all_files_path_list, subdir_list), total=len(subdir_list)):
-            # 获取全球数据集
-            dataset = load_nc_data(file)
-            # 获取区间内部分经纬度
-            _lat, _lon = get_part_lat_lon(dataset, start_lat, start_lon)
-            # 制作csv文件
-            make_csv_file(dataset, _lat, _lon, sub)
-        # 按月合并csv文件
-        merge_csv_file(process_path)
+        shutil.rmtree('processed')
 
+    o = OperateFiles((120.5, 121.5), (34.75, 35.0))
+    o.read_data('raw_data')

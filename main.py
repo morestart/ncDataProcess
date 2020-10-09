985ada17a7b10f4c66167d6c26813e316cd156c5
cattree <morestarforyou@gmail.com>
2020-10-09 13:21:55 +0800

    修复在删除processed文件夹和找不到此文件夹的错误

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import shutil
 from utils.utils import OperateFiles
 
 if __name__ == '__main__':
+
+    data_root_path = 'raw_data'
+
     try:
         os.mkdir('processed')
     except FileExistsError:
         shutil.rmtree('processed')
+        os.mkdir('processed')
 
-    o = OperateFiles((120.5, 121.5), (34.75, 35.0))
+    o = OperateFiles((120.5, 121), (35.0, 35.5))
     o.read_data('raw_data')

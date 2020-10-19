6f260adc061ade0fceb745ff041530113e93b899
cattree <morestarforyou@gmail.com>
2020-10-19 10:11:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import os
-import shutil
-from utils.utils import OperateFiles
-import yaml
+from utils.utils import nc_to_csv
+
 
 if __name__ == '__main__':
 
-    with open('config.yml', 'r') as f:
-        data = f.read()
-
-    content = yaml.load(data, Loader=yaml.FullLoader)
-
-    data_root_path = content['root_path']
-
-    try:
-        os.mkdir('processed')
-    except FileExistsError:
-        shutil.rmtree('processed')
-        os.mkdir('processed')
-
-    o = OperateFiles(content['lon'], content['lat'])
-    o.read_data('raw_data')
+    nc_to_csv()
